61a913e5f754c7acec11d10e9b88fb9bfc62c54f
kaustubh0201 <55658401+kaustubh0201@users.noreply.github.com>
2020-04-20 14:55:09 +0530

    Update MainActivity.java

--- Simple app/MainActivity.java
+++ Simple app/MainActivity.java
@@ -1,1 +1,36 @@
+package com.example.myfirstapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mtruebutton;
+    private Button mfalsebutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mtruebutton = (Button) findViewById(R.id.truebutton);
+        mfalsebutton = (Button) findViewById(R.id.falsebutton);
+        mtruebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mfalsebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
